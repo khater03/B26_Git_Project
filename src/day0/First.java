ea72b67407f05168957b4dc1415e6a405df3020a
Saba <khater03@yahoo.com>
2022-04-12 13:00:11 -0400

    EDITED FIRST FILE

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,8 +2,16 @@ package day0;
 
 public class First {
 
-         // TODO: WORKS ON THIS TOMORROW
-    // ADD SOME COOL CODE
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working...");
+
+    }
+
+    // extra line 1
+    // extra line 2
+    // extra line 3
+
 
 
     }
